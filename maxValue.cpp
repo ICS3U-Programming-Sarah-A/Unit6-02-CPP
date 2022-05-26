d5cbf098c09003210702dd4e5653152678cb1420
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-05-26 22:32:05 +0000

    Made a program that displays the max value of 10 random generated numbers.

--- maxValue.cpp
+++ maxValue.cpp
@@ -8,41 +8,50 @@
 #include <iostream>
 #include <array>
 #include <random>
+#include <ctime>
 
 // declare constants
 const int MAX_SIZE = 10;
 const int MIN_NUM = 0;
 const int MAX_NUM = 100;
 
-// function upheraaa which I need to do 
-int findMaxValue(std::array<int, MAX_SIZE)
+// this function checks to see the largest item in the array
+int findMaxValue(std::array<int, MAX_SIZE > numRandom) {
+    int counter, max;
+    max = numRandom[0];
 
-
-
-
-
+    for (counter = 0; counter < MAX_SIZE; counter++) {
+        if (max < numRandom[counter]) {
+            max = numRandom[counter];
+        }
+    }
+    return max;
+}
 
 
 int main() {
-    // initalize counter
+    // initialize counter
     int counter = 0;
-    
+
     // declare variable
-    std::array<int, MAX_ARRAY_SIZE > randomNum;
+    std::array<int, MAX_SIZE > randomNum;
     int maxValue;
-    
+
     // generates a random number
     srand(time(NULL));
-    
+
     // determine the random number generated then displays it to the user.
-    for (counter = 1; counter < MAX_SIZE; counter++) {
-        if maxValue <
+    for (counter = 0; counter < MAX_SIZE; counter++) {
         int randomNumber = (rand() % (MIN_NUM, MIN_NUM + MAX_NUM));
-        
+
         randomNum[counter] = randomNumber;
         std::cout << randomNum[counter];
         std::cout << " is added to the list at position " << counter;
         std::cout << std::endl;
     }
 
+    // calls function & display the max value
+    maxValue = findMaxValue(randomNum);
+    std::cout << std::endl;
+    std::cout << "The max value is: " << maxValue;
 }
